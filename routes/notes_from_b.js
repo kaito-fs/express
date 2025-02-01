43e6b61c0e4e69a05a5ed1cb9bc0a7d4b6623bf3
kaito-fs <kaito881507@gmail.com>
2025-02-02 00:08:15 +0900

    add env

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -3,13 +3,12 @@ var router = express.Router();
 const cors = require('cors'); // corsミドルウェアを追加
 require('dotenv').config();
 
-
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
-// corsミドルウェアを使用
+//corsミドルウェアを使用
 router.use(cors());
 
 router.get('/', async (req, res) => {
@@ -17,9 +16,9 @@ router.get('/', async (req, res) => {
 const database = client.db('notes');
 const notes = database.collection('notes');
 
-// idが１のドキュメントを取得
-const query = { id: 2 };
-const note = await notes.findOne(query);
+
+// 全てのドキュメントを取得
+const note = await notes.find({}).toArray();
 
 res.json(note);
 })
